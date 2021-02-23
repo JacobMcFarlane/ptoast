6f89b4980a5e21413c3c1bd48817bf2f58cfb5d2
jacobmcf <jacobmcf@student.ubc.ca>
2021-02-23 11:51:02 -0800

    Initial package setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pypkgs_toast in a project::
+
+    import ptoast
---
+++ tests/test_ptoast.py
@@ -0,0 +1,5 @@
+from ptoast import __version__
+from ptoast import ptoast
+
+def test_version():
+    assert __version__ == '0.1.0'
